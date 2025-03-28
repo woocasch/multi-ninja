0bd75a778ac94d8085d7b4967e9690b090a43944
Łukasz Nowakowski <lukasz@lnow.pl>
2025-03-28 22:06:06 +0100

    Add routing and home page.

---
+++ front/src/home/HomePage.tsx
@@ -0,0 +1,11 @@
+import './HomePage.css';
+
+function HomePage() {
+    return (
+        <div className="HomePage">
+            Welcome to MultiNinja
+        </div>
+    );
+};
+
+export default HomePage;
