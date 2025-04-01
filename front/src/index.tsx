2bb7c9939945d3f28f6eeee422fea9ffe80bbe31
Łukasz Nowakowski <lukasz@lnow.pl>
2025-04-01 23:42:37 +0200

    Remove old game board.

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,21 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { createBrowserRouter, RouterProvider } from "react-router";
-import GameBoard from './game_board/game_board';
 import PlayComponent from './play/play';
-
-function Root() {
-  return <h1>Hello world</h1>;
-}
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: App
-  },
-  {
-    path: 'game_board',
-    Component: GameBoard
   },
   {
     path: 'play',
