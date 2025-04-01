5133a219fbd4c4f0e0149a72aa0f05de321f32ac
Łukasz Nowakowski <lukasz@lnow.pl>
2025-04-01 22:48:31 +0200

    Fix generation of factors.

--- front/src/play/questions_generator.ts
+++ front/src/play/questions_generator.ts
@@ -19,8 +19,8 @@ class QuestionsGeneratorService {
     private allQuestions: Combination[] = [];
 
     constructor() {
-        for (let i = 0; i < 10; i++) {
-            for (let j = 0; j < 10; j++) {
+        for (let i = 1; i <= 10; i++) {
+            for (let j = 1; j <= 10; j++) {
                 const combination: Combination = {
                     leftHand: i,
                     rightHand: j,
