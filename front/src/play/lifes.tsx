67652050e4e8e42b7ac044267f5f02f242559ef6
Łukasz Nowakowski <lukasz@lnow.pl>
2025-04-02 19:56:47 +0200

    Improve lifes display.

--- front/src/play/lifes.tsx
+++ front/src/play/lifes.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useMemo } from "react";
+import './lifes.css';
+import lostLifeImage from '../assets/shuriken-empty.svg';
+import remainingLifeImage from '../assets/shuriken-full.svg';
 
 export interface Properties {
     lifesLost: number;
@@ -6,9 +9,18 @@ export interface Properties {
 }
 
 export default function LifesComponent(props: Properties) {
+    const lifesRemaining = useMemo(() => {
+        return props.lifesAvailable - props.lifesLost;
+    }, [props.lifesLost, props.lifesAvailable]);
+
     return (
-        <div>
-            {props.lifesLost}/{props.lifesAvailable}
+        <div class="lifes-display">
+            {[...Array(lifesRemaining)].map((x, i) => (
+                <img key={i} src={remainingLifeImage} style={{ width: '20px' }} />
+            ))}
+            {[...Array(props.lifesLost)].map((x, i) => (
+                <img key={i} src={lostLifeImage} style={{ width: '20px' }} />
+            ))}
         </div>
     );
 }
