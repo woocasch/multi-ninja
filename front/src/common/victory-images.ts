c6c10e1696a4d8114b446fdcc4b78dfefd76c928
Łukasz Nowakowski <woocasch@users.noreply.github.com>
2025-04-27 22:23:14 +0200

    Add random reward after flawless victory (#17)
    * Add all reward images.
    
    * Add selection of random image.

---
+++ front/src/common/victory-images.ts
@@ -0,0 +1,181 @@
+import amber from '../assets/victory-images/amber.png';
+import angelo from '../assets/victory-images/angelo.png';
+import ash from '../assets/victory-images/ash.png';
+import barley from '../assets/victory-images/barley.png';
+import bea from '../assets/victory-images/bea.png';
+import belle from '../assets/victory-images/belle.png';
+import berry from '../assets/victory-images/berry.png';
+import bibi from '../assets/victory-images/bibi.png';
+import bo from '../assets/victory-images/bo.png';
+import bonnie from '../assets/victory-images/bonnie.png';
+import brock from '../assets/victory-images/brock.png';
+import bull from '../assets/victory-images/bull.png';
+import buster from '../assets/victory-images/buster.png';
+import buzz from '../assets/victory-images/buzz.png';
+import buzzlightyear from '../assets/victory-images/buzzlightyear.png';
+import byron from '../assets/victory-images/byron.png';
+import carl from '../assets/victory-images/carl.png';
+import charlie from '../assets/victory-images/charlie.png';
+import chester from '../assets/victory-images/chester.png';
+import chuck from '../assets/victory-images/chuck.png';
+import clancy from '../assets/victory-images/clancy.png';
+import colette from '../assets/victory-images/colette.png';
+import colt from '../assets/victory-images/colt.png';
+import cordelius from '../assets/victory-images/cordelius.png';
+import crow from '../assets/victory-images/crow.png';
+import darryl from '../assets/victory-images/darryl.png';
+import doug from '../assets/victory-images/doug.png';
+import draco from '../assets/victory-images/draco.png';
+import dynamike from '../assets/victory-images/dynamike.png';
+import edgar from '../assets/victory-images/edgar.png';
+import eightbit from '../assets/victory-images/eightbit.png';
+import elprimo from '../assets/victory-images/elprimo.png';
+import emz from '../assets/victory-images/emz.png';
+import eve from '../assets/victory-images/eve.png';
+import fang from '../assets/victory-images/fang.png';
+import frank from '../assets/victory-images/frank.png';
+import gale from '../assets/victory-images/gale.png';
+import gene from '../assets/victory-images/gene.png';
+import gray from '../assets/victory-images/gray.png';
+import griff from '../assets/victory-images/griff.png';
+import grom from '../assets/victory-images/grom.png';
+import gus from '../assets/victory-images/gus.png';
+import hank from '../assets/victory-images/hank.png';
+import jacky from '../assets/victory-images/jacky.png';
+import janet from '../assets/victory-images/janet.png';
+import jessie from '../assets/victory-images/jessie.png';
+import juju from '../assets/victory-images/juju.png';
+import kenji from '../assets/victory-images/kenji.png';
+import kit from '../assets/victory-images/kit.png';
+import larrylawrie from '../assets/victory-images/larrylawrie.png';
+import leon from '../assets/victory-images/leon.png';
+import lily from '../assets/victory-images/lily.png';
+import lola from '../assets/victory-images/lola.png';
+import lou from '../assets/victory-images/lou.png';
+import maisie from '../assets/victory-images/maisie.png';
+import mandy from '../assets/victory-images/mandy.png';
+import max from '../assets/victory-images/max.png';
+import meeple from '../assets/victory-images/meeple.png';
+import meg from '../assets/victory-images/meg.png';
+import melodie from '../assets/victory-images/melodie.png';
+import mico from '../assets/victory-images/mico.png';
+import moe from '../assets/victory-images/moe.png';
+import mortis from '../assets/victory-images/mortis.png';
+import mrp from '../assets/victory-images/mrp.png';
+import nani from '../assets/victory-images/nani.png';
+import nita from '../assets/victory-images/nita.png';
+import ollie from '../assets/victory-images/ollie.png';
+import otis from '../assets/victory-images/otis.png';
+import pam from '../assets/victory-images/pam.png';
+import pearl from '../assets/victory-images/pearl.png';
+import penny from '../assets/victory-images/penny.png';
+import piper from '../assets/victory-images/piper.png';
+import poco from '../assets/victory-images/poco.png';
+import rico from '../assets/victory-images/rico.png';
+import rosa from '../assets/victory-images/rosa.png';
+import rt from '../assets/victory-images/rt.png';
+import ruffs from '../assets/victory-images/ruffs.png';
+import sam from '../assets/victory-images/sam.png';
+import sandy from '../assets/victory-images/sandy.png';
+import shade from '../assets/victory-images/shade.png';
+import shelly from '../assets/victory-images/shelly.png';
+import spike from '../assets/victory-images/spike.png';
+import sprout from '../assets/victory-images/sprout.png';
+import squeak from '../assets/victory-images/squeak.png';
+import stu from '../assets/victory-images/stu.png';
+import surge from '../assets/victory-images/surge.png';
+import tara from '../assets/victory-images/tara.png';
+import tick from '../assets/victory-images/tick.png';
+import willow from '../assets/victory-images/willow.png';
+
+export const images: string[] = [
+  amber,
+  angelo,
+  ash,
+  barley,
+  bea,
+  belle,
+  berry,
+  bibi,
+  bo,
+  bonnie,
+  brock,
+  bull,
+  buster,
+  buzz,
+  buzzlightyear,
+  byron,
+  carl,
+  charlie,
+  chester,
+  chuck,
+  clancy,
+  colette,
+  colt,
+  cordelius,
+  crow,
+  darryl,
+  doug,
+  draco,
+  dynamike,
+  edgar,
+  eightbit,
+  elprimo,
+  emz,
+  eve,
+  fang,
+  frank,
+  gale,
+  gene,
+  gray,
+  griff,
+  grom,
+  gus,
+  hank,
+  jacky,
+  janet,
+  jessie,
+  juju,
+  kenji,
+  kit,
+  larrylawrie,
+  leon,
+  lily,
+  lola,
+  lou,
+  maisie,
+  mandy,
+  max,
+  meeple,
+  meg,
+  melodie,
+  mico,
+  moe,
+  mortis,
+  mrp,
+  nani,
+  nita,
+  ollie,
+  otis,
+  pam,
+  pearl,
+  penny,
+  piper,
+  poco,
+  rico,
+  rosa,
+  rt,
+  ruffs,
+  sam,
+  sandy,
+  shade,
+  shelly,
+  spike,
+  sprout,
+  squeak,
+  stu,
+  surge,
+  tara,
+  tick,
+  willow,
+];
